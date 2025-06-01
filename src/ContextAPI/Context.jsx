2b4792e77e60ca258cd3640e0a8ee2b792cf86a9
Krish-006-19 <krish.ramena24@vit.edu>
2025-06-01 06:50:34 +0530

    latest changes

--- src/ContextAPI/Context.jsx
+++ src/ContextAPI/Context.jsx
@@ -1,34 +1,24 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useState, useEffect } from "react";
 
-const PageContext = createContext(null)
+const PageContext = createContext();
 
-export function PageProvider({children}){
-    const [type, setType] = useState('')
-    const [search, setSearch] = useState('')
-    const [bool, setBool] = useState(false)
-    const [subj, setSubj] = useState('')
-    const [cart, setCart] = useState([])
-    return(
-        <PageContext.Provider 
-        value={
-            {
-                type,
-                setType,
-                search,
-                setSearch,
-                bool,
-                setBool,
-                subj,
-                setSubj,
-                cart, 
-                setCart
-            }
-        }>
-            {children}
-        </PageContext.Provider>
-    )
-}
+export const PageProvider = ({ children }) => {
+  const [type, setType] = useState(() => localStorage.getItem("type") || "");
+  const [search, setSearch] = useState(() => localStorage.getItem("search") || "");
+  const [bool, setBool] = useState(() => JSON.parse(localStorage.getItem("bool")) || false);
+  const [subj, setSubj] = useState(null);
 
-export function usePage(){
-    return useContext(PageContext)
-}
+  useEffect(() => {
+    localStorage.setItem("type", type);
+    localStorage.setItem("search", search);
+    localStorage.setItem("bool", JSON.stringify(bool));
+  }, [type, search, bool]);
+
+  return (
+    <PageContext.Provider value={{ type, setType, search, setSearch, bool, setBool, subj, setSubj }}>
+      {children}
+    </PageContext.Provider>
+  );
+};
+
+export const usePage = () => useContext(PageContext);
